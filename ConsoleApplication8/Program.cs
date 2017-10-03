fa6b2ef45936c546257bbfff89970a19e52a052b
ines <ines.bielecka@gmail.com>
2017-10-03 21:17:42 +0200

    Already made stages until Radio, now thinking about refactoring existing code in stage Szyby and Klima

--- ConsoleApplication8/Program.cs
+++ ConsoleApplication8/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Linq;
+
 public class Auto
 {
-   
     protected int _cena = 10000;
 
-    public string Markaa { set; get; }
+    public string Marka { set; get; }
     public enum Marki
     {
         Audi,
@@ -27,26 +28,14 @@ public class Auto
         Renault
     }
     public string Lakier { set; get; }
-
     public int Rocznik { set; get; }
-    
     public int Przebieg { set; get; }
-   
-            //if (value <= 0)
-            //{
-            //    Console.WriteLine("Przebieg samochodu nie może być mniejszy bądź równy 0.");
-            //}
-
     public double Silnik { get; set; }
-
     public bool Klima { get; set; }
     public bool Szyby { get; set; }
     public bool Radio { get; set; }
     public bool Zamek { get; set; }
     public int Cena { get; set; }
-
-
-
 
     public void JakaMarka()
     {
@@ -54,19 +43,35 @@ public class Auto
 
     }
 
-
-
     public void Paint()
     {
         Console.WriteLine("Jaki kolor lakieru?");
 
     }
 
-
-
     public void AskForYearOfCarProducion()
     {
         Console.WriteLine("Jaki jest rocznik samochodu? (yyyy)");
+    }
+
+    public void AskForMileageOfCar()
+    {
+        Console.WriteLine("Jaki jest przebieg samochodu?");
+    }
+
+    public void AskForEngine()
+    {
+        Console.WriteLine("Jaką pojemność ma silnik?");
+    }
+
+    public void AskForAC()
+    {
+        Console.WriteLine("Czy samochód posiada klimatyzację? T/N");
+    }
+
+    public void AskForElectricWindows()
+    {
+        Console.WriteLine("Czy samochód posiada elektrycznie otwierane okna? T/N");
     }
 
 }
@@ -93,64 +98,117 @@ class Program : Auto
 
             if (int.TryParse(ChosenBrand, out brandIndex) && brandIndex >= 0 && brandIndex <= Values.Length)
             {
+                m.Marka = ChosenBrand;
                 flag = false;
             }
             else
             {
                 Console.WriteLine("Wpisz poprawny numer.");
             }
-            
         }
         while (flag);
-
-        //else 
-        //{
-        //    Console.WriteLine("Wpisz poprawny numer.");
-        //}
-
-        //switch (int.Parse(ChosenBrand))
-        //{
-        //    case Values:
-        //        break;
-        //    default:
-        //        Console.WriteLine("Wpisz poprawny numer.");
-        //        break;
-        //}
-
-        //if ( int.Parse(ChosenBrand) != )
-        //{
-        //    Console.WriteLine("Wpisz poprawny numer.");
-        //}
-
-
 
         Auto p = new Auto();
         p.Paint();
         string Paintcolour = Console.ReadLine();
-
+        p.Lakier = Paintcolour;
 
         Auto y = new Auto();
         y.AskForYearOfCarProducion();
-       
+        bool flag2 = true;
         do
         {
-            
             string YearofProduction = Console.ReadLine();
             int i = 0;
 
             if (int.TryParse(YearofProduction, out i) && i > 1950 && i <= DateTime.Now.Year)
             {
                 y.Rocznik = i;
-                flag = false;
+                flag2 = false;
             }
             else
             {
                 Console.WriteLine("Wpisz poprawny rocznik. (yyyy)");
             }
         }
-        while (flag);
-       
+        while (flag2);
+
+        Auto mileage = new Auto();
+        mileage.AskForMileageOfCar();
+        bool flag3 = true;
+        do
+        {
+            string MileageofCar = Console.ReadLine();
+            int mile = 0;
+            if (int.TryParse(MileageofCar, out mile) && mile > 0)
+            {
+                mileage.Przebieg = mile;
+                flag3 = false;
+            }
+            else
+            {
+                Console.WriteLine("Wpisz poprawny przebieg.");
+            }
         }
+        while (flag3);
+
+        Auto engine = new Auto();
+        engine.AskForEngine();
+        bool flag4 = true;
+        do
+        {
+            string EngineCapacity = Console.ReadLine();
+            double capacity = 0;
+            if (double.TryParse(EngineCapacity, out capacity) && capacity >= 1)
+            {
+                engine.Silnik = capacity;
+                flag4 = false;
+            }
+        }
+        while (flag4);
+
+        Auto airconditioning = new Auto();
+        airconditioning.AskForAC();
+        bool flag5 = true;
+        do
+        {
+            string AirConditioningIncluding = Console.ReadLine().ToUpper();
+            switch (AirConditioningIncluding)
+            {
+                case "T":
+                    flag5 = false;
+                    break;
+                case "N":
+                    flag5 = false;
+                    break;
+                default:
+                    Console.WriteLine("Udziel poprawnej odpowiedzi. T/N");
+                    break;
+            }
+        }
+        while (flag5);
+
+        Auto electricwindows = new Auto();
+        electricwindows.AskForElectricWindows();
+        bool flag6 = true;
+        do
+        {
+            string ElectricWindowsInclud = Console.ReadLine().ToUpper();
+            switch (ElectricWindowsInclud)
+            {
+                case "T":
+                    flag6 = false;
+                    break;
+                case "N":
+                    flag6 = false;
+                    break;
+                default:
+                    Console.WriteLine("Udziel poprawnej odpowiedzi. T/N");
+                    break;
+            }
+        }
+        while (flag6);
+    }
 }
 
 
